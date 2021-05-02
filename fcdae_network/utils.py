a3e81cf0693dd1e688e3694eeb2eabaffb2cabaa
christodouloskech <christodouloskech@gmail.com>
2021-05-02 15:53:59 +0300

    Fix utils.py

--- fcdae_network/utils.py
+++ fcdae_network/utils.py
@@ -1,66 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import scipy.io as sio
-
-def loadData(dataFolder, fileName, extension, inputSignalTag, groundTruthSignalTag, \
-             groundTruthSpikeTimeTrainTag):
-  mat = sio.loadmat(dataFolder + '/' + fileName + '.' + extension)
-
-  y = mat[inputSignalTag]
-  groundTruth = mat[groundTruthSignalTag]
-  
-  groundTruthSpikeTimes = None
-  if groundTruthSpikeTimeTrainTag != None:
-      mat = sio.loadmat(dataFolder + '/' + fileName + '_spikes' + '.' + extension)
-      groundTruthSpikeTimes = mat[groundTruthSpikeTimeTrainTag]
-
-  return y, groundTruth, groundTruthSpikeTimes
-
-def loadSpikeTrainData(dataFolder, fileName, extension, inputSignalTag, groundTruthSignalTag):
-  mat = sio.loadmat(dataFolder + '/' + fileName + '_spikes.' + extension)
-
-  return mat['groundTruthSpikeTimeTrain']
-
-def loadTestData(dataFolder, fileName, extension, inputSignalTag, groundTruthSignalTag):
-  mat = sio.loadmat(dataFolder + '/' + fileName + '.' + extension)
-
-  y = mat['out_data']
-
-  return y
-
-def resizeData(data, inputSize, stride):
-  output = []
-  for i in range(0, len(data), stride):
-    if i + inputSize > len(data):
-      break
-    output.append(data[i:i+inputSize])
-  output_array = np.transpose(np.stack(output, axis = 1))
-  
-  return output_array
-
-
-def resizeMultiChannelData(data, inputSize, stride):
-  output = []
-  for i in range(0, len(data), stride):
-    if i + inputSize > len(data):
-      break
-    output.append(data[i:i+inputSize, :])
-  output_array = np.stack(output, axis = 1)
-  
-  output_array = np.swapaxes(output_array, 0, 1)
-  
-  return output_array
-
-def resizeToTimeseries(data):
-  return data.ravel()
-
-def resizeToTimeseriesMultiChannel(data):
-    output_array  = np.zeros((data.shape[0] * data.shape[1], data.shape[2]))
-    for i in range(0, data.shape[2]):
-        output_array[:, i] = data[:, :, i].ravel()
-        
-    return output_array
-    
     
 def calculateSNRImprovement(testX, testy, predictions):
     SNR_imp = 10 * np.log10(np.sum(np.square(testX - testy), axis = 0) \
