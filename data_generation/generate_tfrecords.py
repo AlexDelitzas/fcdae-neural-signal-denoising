f22fa966663bd525a8b6d594e8700a1165cb0047
christodouloskech <christodouloskech@gmail.com>
2021-05-04 16:32:14 +0300

    Add CPU support for generate_tfrecords.py

--- data_generation/generate_tfrecords.py
+++ data_generation/generate_tfrecords.py
@@ -8,9 +8,10 @@ from utils import loadData, resizeMultiChannelData, resizeData
 # Change device to "CPU" if you want to run tensorflow on your CPU.
 use_device = "GPU"
 
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
-config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+if (use_device == "GPU"):
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+    config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 class TFRecordDatasetBuilder:
     
